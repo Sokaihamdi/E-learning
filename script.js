d6e0dfae058365df925f0cf75e526b8fe0d93a55
Kossay <125670885+Sokaihamdi@users.noreply.github.com>
2024-10-04 15:42:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const sbtn = document.getElementById("submit-button")
+submit-button
+const submit = (e) =>{
+
+    e.preventDefault()
+    alert("Sing up successed");
+}
+sbtn.addEventListener("click", submit)  #
